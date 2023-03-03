ccb77653682a647c8c69987aa56650a517036676
Matthew Dolan <mdolancode@gmail.com>
2023-03-03 14:50:50 -0800

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Matthew Dolan on 2023-03-03.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
