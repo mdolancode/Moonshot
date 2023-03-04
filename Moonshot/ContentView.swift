39d65c7576c072d3d64320291fbc97b69ada8acb
Matthew Dolan <mdolancode@gmail.com>
2023-03-03 16:01:10 -0800

    Project 8, part 1 - How ScrollView lets us work with scrolling data including Lazy Stacks.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,14 +7,31 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("Future")
-                .resizable()
-                .scaledToFit()
-                .frame(width: geo.size.width * 0.8)
-                .frame(width: geo.size.width, height: geo.size.height)
+        // You can make the scrolls horizontal or vertical.
+        ScrollView(.horizontal) {
+            // Lazy Stacks take up all the available space with width and height. Regular stacks take up the available content space.
+            LazyHStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
     }
 }
