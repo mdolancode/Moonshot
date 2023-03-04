724f039c7c0ed08ac76532ce28507508b882587e
Matthew Dolan <mdolancode@gmail.com>
2023-03-04 14:53:03 -0800

    Project 8, part 2 - Using generics to load any kind of Codable data.

---
+++ Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Matthew Dolan on 2023-03-04.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
