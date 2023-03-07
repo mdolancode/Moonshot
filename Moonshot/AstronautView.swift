4b05e8899dc6195d4c2f4b9732bd2c2d2da58270
Matthew Dolan <mdolancode@gmail.com>
2023-03-07 07:18:07 -0800

    Project 8, part 3 - Finishing up with one last view.

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,38 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Matthew Dolan on 2023-03-06.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+                
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["armstrong"]!)
+            .preferredColorScheme(.dark)
+    }
+}
